e2d07a5f67159be9e7e65cd21ff4584d8c0d291a
Reuben Coutinho <reuben211999@icloud.com>
2021-07-29 19:50:48 +0530

    Updated Login Screen and Sign up Screen

---
+++ app/src/main/java/com/project22/myapplication/SignUpActivity.kt
@@ -0,0 +1,44 @@
+package com.project22.myapplication
+
+import android.os.Build
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        try {
+            // TODO :- To Hide the Toolbar which Comes by Default
+            this.supportActionBar!!.hide()
+        }
+        catch (e: NullPointerException) {
+        }
+
+        // TODO :- To Make Fragment Visible in Full Screen
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        // TODO :- To go back to Login Screen
+        toolbar_back_to_Login.setNavigationOnClickListener { onBackPressed() }
+
+
+
+    }
+
+
+
+}
