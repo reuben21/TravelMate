cc993917d7c7d36bc36a1cbb483503387f0d72e8
reuben21 <reuben211999@icloud.com>
2021-09-19 20:16:24 +0530

    updated code

--- app/src/main/java/com/project22/myapplication/screens/TravelDestinationForm.kt
+++ app/src/main/java/com/project22/myapplication/screens/TravelDestinationForm.kt
@@ -1,12 +1,33 @@
 package com.project22.myapplication.screens
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.project22.myapplication.R
 
 class TravelDestinationForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_travel_destination_form)
+
+        try {
+            // TODO :- To Hide the Toolbar which Comes by Default
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
+        // TODO :- To Make Fragment Visible in Full Screen
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
+
 }
