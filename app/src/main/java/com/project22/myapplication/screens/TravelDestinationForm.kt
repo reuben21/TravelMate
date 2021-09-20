a1443a9d6f48e6bf3e7afdbb2df4c182badf9185
reuben21 <reuben211999@icloud.com>
2021-09-20 07:18:13 +0530

    Updated Code with Google Autocomplete API

--- app/src/main/java/com/project22/myapplication/screens/TravelDestinationForm.kt
+++ app/src/main/java/com/project22/myapplication/screens/TravelDestinationForm.kt
@@ -1,13 +1,32 @@
 package com.project22.myapplication.screens
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.project22.myapplication.R
+import kotlinx.android.synthetic.main.activity_travel_destination_form.*
+import java.util.*
 
-class TravelDestinationForm : AppCompatActivity() {
+class TravelDestinationForm : AppCompatActivity()  {
+    private val AUTOCOMPLETE_REQUEST_CODE = 1
+    var buttonLocation:String = ""
+    val fields = listOf(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_travel_destination_form)
@@ -28,6 +47,66 @@ class TravelDestinationForm : AppCompatActivity() {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
             )
         }
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), getString(R.string.google_maps_key), Locale.US);
+        }
+
+
+        inputOrigin.setOnClickListener {
+            val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this)
+            buttonLocation = "inputOrigin"
+            startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+        }
+
+        inputDestination.setOnClickListener {
+            val intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this)
+            buttonLocation = "inputDestination"
+            startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+        }
+
+        SubmitButtonTravelForm.setOnClickListener {
+            Log.d("FORM VALUES","${inputDestination.text}  ${inputOrigin.text}")
+
+        }
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    data?.let {
+                        val place = Autocomplete.getPlaceFromIntent(data)
+                        Log.i("TAG", "Place: ${place.name}, ${place.latLng?.longitude}")
+                        if (buttonLocation == "inputDestination") {
+                            (inputDestination as TextView).text = place.name
+                        } else if (buttonLocation == "inputOrigin") {
+                            (inputOrigin as TextView).text = place.name
+                        }
+
+
+                    }
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    // TODO: Handle the error.
+                    data?.let {
+                        val status = Autocomplete.getStatusFromIntent(data)
+                        status.statusMessage?.let { it1 -> Log.i("TAG", it1) }
+                    }
+                }
+                Activity.RESULT_CANCELED -> {
+                    // The user canceled the operation.
+                }
+            }
+            return
+        }
+
+    }
+
+
 
 }
